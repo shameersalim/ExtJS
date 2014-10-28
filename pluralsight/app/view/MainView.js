a72e447d1a510f60a5b6d6015f0db93692d47385
touchydeer36 <shameersalim@outlook.com>
2014-10-27 21:21:39 -0400

    Pluralsight example

---
+++ pluralsight/app/view/MainView.js
@@ -0,0 +1,50 @@
+﻿Ext.define("SE.view.MainView", {
+    extend: "Ext.container.Viewport",
+    layout: {
+        type: "border",
+        //align: "stretch"
+    },
+    items: [
+        {
+            region: 'west',
+            layout: {
+                type: "vbox",
+                align: "stretch"
+            },
+            flex: 1,
+            split: true,
+            items: [
+             /*   {
+                    xtype: "sessiongridpanel",
+                    flex: 3
+                },*/
+                {
+                    xtype: 'panel',
+                    html: '<b>Sessions Panel</b>',
+                    flex: 2
+                },
+                {
+                    xtype: 'splitter',
+                    width: 1
+                },
+                {
+                    xtype: 'panel',
+                    html: '<b>Presenters</b>',
+                    flex: 1
+                }
+            ]
+
+        },
+        {
+            region: 'center',
+            xtype: 'panel',
+            html: '<b>Session Details</b>',
+            flex: 1,
+            title: "Details",
+            collapsible: true,
+            collapseDirection: "right",
+            collapsed: false
+        }
+    ]
+
+});
